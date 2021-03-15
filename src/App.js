5210be3bf08c9d299d2b8badfbf5f16a908e0f2c
vinodkv2511 <vinodkv2511@gmail.com>
2021-03-15 17:11:28 +0530

    Setup react-query provider

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import { 
+  BrowserRouter as Router,
+  Redirect,
+  Switch,
+  Route
+} from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+
 import './App.scss';
+import Tasks from './containers/tasks/tasks';
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        Hello
+
+        <Router>
+          <Switch>
+            <Route exact path="/"> <Redirect to="/tasks" /> </Route>  
+            <Route exact path="/tasks">
+              <Tasks />
+            </Route>
+          </Switch>    
+        </Router>
+      </div>
+    </QueryClientProvider>
   );
 }
 
