daeadbf3a1f22695137d9db069857ed498f393ff
vinodkv2511 <vinodkv2511@gmail.com>
2021-03-16 19:45:45 +0530

    Fix update API call

--- src/apis/tasks.js
+++ src/apis/tasks.js
@@ -3,3 +3,16 @@ import client from './client';
 export const fetchTasks = (cancelToken) => client.get('/tasks/list', {
     cancelToken: cancelToken.token
 });
+
+export const postTaskUpdate = (data) => {
+
+    let parsedData = new FormData();
+    for( let key in data) {
+        parsedData.append(key, `${data[key]}`);
+    }
+    return client.post('/tasks/update', parsedData, {
+        headers: {
+            'Content-Type': 'multipart/form-data'
+        }
+    })   
+}
