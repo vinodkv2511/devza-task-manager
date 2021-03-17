ab5728bb8efc9d5a2eecd1bc32282fb537b0e435
vinodkv2511 <vinodkv2511@gmail.com>
2021-03-17 08:06:47 +0530

    Update button to accept class name

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -2,9 +2,9 @@ import './button.scss';
 
 
 // TODO- Implement primary and text only styles
-const Button = ({onClick, label, isDanger}) => {
+const Button = ({onClick, label, isDanger, className}) => {
     return (
-        <button className={`button secondary ${isDanger && 'danger'}`} onClick={onClick}>{label}</button>
+        <button className={`button secondary ${isDanger && 'danger'} ${className}`} onClick={onClick}>{label}</button>
     )
 }
 
