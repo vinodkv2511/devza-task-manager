f422d64ad3bf4cf7bb367e444ea0f6ffbb860127
vinodkv2511 <vinodkv2511@gmail.com>
2021-03-17 11:40:55 +0530

    Allow button to be disabled

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -2,9 +2,12 @@ import './button.scss';
 
 
 // TODO- Implement primary and text only styles
-const Button = ({onClick, label, isDanger, className}) => {
+const Button = ({onClick, label, isDanger, className, disabled}) => {
     return (
-        <button className={`button secondary ${isDanger && 'danger'} ${className}`} onClick={onClick}>{label}</button>
+        <button
+            className={`button secondary ${isDanger && 'danger'} ${className} ${disabled && 'disabled'}`} 
+            onClick={() => { (!disabled && onClick) && onClick()} }
+        >{label}</button>
     )
 }
 
